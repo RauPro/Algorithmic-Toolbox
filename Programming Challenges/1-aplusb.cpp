abef851ea24864b2e70f23dca20fc677ae3ee8b4
RauPro <00012119@uca.edu.sv>
2020-10-07 16:39:50 -0600

    First commit

---
+++ Programming Challenges/1-aplusb.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Fast ios::sync_with_stdio(0); cin.tie(0);
+
+typedef long long ll;
+typedef long int li;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+int main(){
+	Fast
+	int a,b;cin>>a>>b;
+	cout<<(a+b)<<endl;
+} 
